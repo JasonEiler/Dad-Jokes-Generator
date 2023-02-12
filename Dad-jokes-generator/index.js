3e7e31c9685a1469c1046e0a187bf13cae548c2d
Jason Eiler <125165647+JasonEiler@users.noreply.github.com>
2023-02-11 19:41:58 -0800

    Add files via upload

---
+++ Dad-jokes-generator/index.js
@@ -0,0 +1,37 @@
+const btnEl = document.getElementById("btn");
+const jokeEl = document.getElementById("joke");
+
+const apiKey = "";
+
+const options = {
+    method: "GET",
+    headers: {
+        "X-Api-Key": apiKey,
+    }
+}
+
+const apiURL = "https://api.api-ninjas.com/v1/dadjokes?limit=1";
+
+async function getjoke(){
+
+try {
+    jokeEl.innerText = "updating...";
+    btnEl.disabled = true;
+    btnEl.innerText = "Loading...";
+    const response = await fetch(apiURL, options);
+    const data = await response.json();
+
+    btnEl.disabled = false;
+    btnEl.innerText = "Tell me a joke";
+
+    jokeEl.innerText = data[0].joke;
+} catch (error) {
+    jokeEl.innerText = "An error happend, please try again later.";
+    btnEl.disabled = false;
+    btnEl.innerText = "Tell me a joke";
+    console.log(error);
+}
+
+}
+
+btnEl.addEventListener("click", getjoke);
